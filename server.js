797ca4b68589e69d9f6e4f654b73d8a7926dec0a
giannispan <giannis.panagiotopoulos@gmail.com>
2017-02-23 22:35:04 +0200

    server.js final

--- server.js
+++ server.js
@@ -5,6 +5,22 @@ var express = require('express')
 // Build the app
 var app = express();
 
+//get request
+app.get('/:time', function (req, res) {
 
-// Start it up!
+//require the url parameter
+var time = req.params.time
+//check if date string is valid
+var d = Date.parse(time)/1000
+if (d > 0) {
+	var date = new Date(time).toDateString()
+	res.send({unix: d, natural:date})
+}
+else {
+var unix = new Date(time*1000).toDateString()
+  res.send({unix: Math.round(time), natural:unix})
+}
+})
+
+// Start server up!
 http.createServer(app).listen(8080);

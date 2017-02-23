94723ed8fcb6b668b20d886319252ddd9e310a03
giannispan <giannis.panagiotopoulos@gmail.com>
2017-02-23 20:51:48 +0200

    remove hello world

--- server.js
+++ server.js
@@ -5,11 +5,6 @@ var express = require('express')
 // Build the app
 var app = express();
 
-// Add some middleware
-app.use(function(request, response) {
-  response.writeHead(200, { "Content-Type": "text/plain" });
-  response.end("Hello World!\n");
-});
 
 // Start it up!
 http.createServer(app).listen(8080);
